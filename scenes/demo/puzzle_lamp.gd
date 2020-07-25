60f3cdbe18f7353a2a96a80879ad1b076b78d8aa
Gary B <gary600playsmc@gmail.com>
2020-07-25 14:48:22 -0600

    More puzzle work

--- scenes/demo/puzzle_lamp.gd
+++ scenes/demo/puzzle_lamp.gd
@@ -6,4 +6,4 @@ export var lit = false
 func _input(ev: InputEvent):
 	if ev.is_action_pressed("interact") and overlaps_body($"/root/demo/character"):
 		$"..".puzzle_num(lamp_number)
-		$light.show()
+		$lit_elem.show()
